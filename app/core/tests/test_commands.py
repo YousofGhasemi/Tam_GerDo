4bf383310125d823f402af680e356835d8973c3f
PC-Yousof <yosef.ghasemi.all@gmail.com>
2025-12-09 16:15:00 +0330

    Test added for waiting for loading database! (first part of TDD)

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,33 @@
+"""
+Test custom Django managment commands.
+"""
+
+from unittest.mock import patch
+
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import SimpleTestCase
+from psycopg2 import OperationalError as psycopg2Error
+
+
+@patch("core.management.commands.wait_for_db.Command.check")
+class CommandTests(SimpleTestCase):
+    """Test commands."""
+
+    def test_wait_for_db(self, patched_check):
+        """Test waiting for database."""
+        patched_check.return_value = True
+
+        call_command("wait_for_db")
+
+        patched_check.assert_called_once_with(database=["default"])
+
+    @patch("time.sleep")
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test Waiting for database when getting OperationalError!"""
+        patched_check.side_effect = (
+            [psycopg2Error] * 2 + [OperationalError] * 3 + [True]  # 6 times!
+        )
+        call_command("wait_for_db")
+        self.assertEqual(patched_check.call_count, 6)
+        patched_check.assert_called_with(database=["default"])
