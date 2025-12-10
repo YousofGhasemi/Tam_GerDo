1780801257c57542d4cfe8e7cb96577d918e17e3
PC-Yousof <yosef.ghasemi.all@gmail.com>
2025-12-10 18:34:44 +0330

    Django user model Done!, super user and normalizing emails and encrypting passwords DONE! :D

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -34,3 +34,20 @@ class ModleTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, "Sample123")
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises ValueError"""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "Test123")
+
+    def test_create_superuser(self):
+        """TEst creating a superuser."""
+
+        user = get_user_model().objects.create_superuser(
+            "test@example.com",
+            "Test@123",
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
