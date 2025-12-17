26de228866918ada81e65bd8a3da9f341edb6b5a
PC-Yousof <yosef.ghasemi.all@gmail.com>
2025-12-17 14:24:48 +0330

    add Tag model with full CRUD API and tests

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,11 +2,11 @@
 Views for recipe APIs
 """
 
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 from recipe import serializers
 
 
@@ -34,3 +34,22 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe"""
 
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    """Manage Tag in the database"""
+
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset for authenticated user"""
+
+        return self.queryset.filter(user=self.request.user).order_by("-name")
